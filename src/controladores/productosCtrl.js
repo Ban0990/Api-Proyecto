3d615d0e8ff4af022346af9638cc5a7b9e2d82b6
Ban0990 <jhalmar0990276743@gmail.com>
2025-11-09 20:20:09 -0500

    Actualizacion producto

--- src/controladores/productosCtrl.js
+++ src/controladores/productosCtrl.js
@@ -2,7 +2,7 @@ import { conmysql } from "../db.js";
 
 export const getProductos = async (req, res) => {
   try {
-    const [rows] = await conmysql.query("SELECT * FROM producto");
+    const [rows] = await conmysql.query("SELECT * FROM PRODUCTO");
     res.json(rows);
   } catch (error) {
     res.status(500).json({ message: "Error al obtener los productos", error });
@@ -11,7 +11,7 @@ export const getProductos = async (req, res) => {
 
 export const getProducto = async (req, res) => {
   try {
-    const [rows] = await conmysql.query("SELECT * FROM producto WHERE id = ?", [req.params.id]);
+    const [rows] = await conmysql.query("SELECT * FROM PRODUCTO WHERE id_producto = ?", [req.params.id]);
     if (rows.length <= 0) return res.status(404).json({ message: "Producto no encontrado" });
     res.json(rows[0]);
   } catch (error) {
@@ -21,12 +21,12 @@ export const getProducto = async (req, res) => {
 
 export const createProducto = async (req, res) => {
   try {
-    const { nombre, descripcion, precio, stock } = req.body;
+    const { nombre, precio, estado } = req.body;
     const [result] = await conmysql.query(
-      "INSERT INTO producto (nombre, descripcion, precio, stock) VALUES (?, ?, ?, ?)",
-      [nombre, descripcion, precio, stock]
+      "INSERT INTO PRODUCTO (id_alimento, precio_unitario, estado) VALUES (?, ?, ?, ?)",
+      [nombre,  precio, estado]
     );
-    res.json({ id: result.insertId, nombre, descripcion, precio, stock });
+    res.json({ id: result.insertId, nombre, precio, estado });
   } catch (error) {
     res.status(500).json({ message: "Error al crear el producto", error });
   }
@@ -36,7 +36,7 @@ export const updateProducto = async (req, res) => {
   try {
     const { nombre, descripcion, precio, stock } = req.body;
     const [result] = await conmysql.query(
-      "UPDATE producto SET nombre = ?, descripcion = ?, precio = ?, stock = ? WHERE id = ?",
+      "UPDATE PRODUCTO SET id_alimento = ?, precio_unitario = ?, estado = ? WHERE id_producto = ?",
       [nombre, descripcion, precio, stock, req.params.id]
     );
     if (result.affectedRows <= 0) return res.status(404).json({ message: "Producto no encontrado" });
@@ -48,7 +48,7 @@ export const updateProducto = async (req, res) => {
 
 export const deleteProducto = async (req, res) => {
   try {
-    const [result] = await conmysql.query("DELETE FROM producto WHERE id = ?", [req.params.id]);
+    const [result] = await conmysql.query("DELETE FROM PRODUCTO WHERE id_producto = ?", [req.params.id]);
     if (result.affectedRows <= 0) return res.status(404).json({ message: "Producto no encontrado" });
     res.json({ message: "Producto eliminado correctamente" });
   } catch (error) {
